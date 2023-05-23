65ff18ca6af597e15a5cdb7f5a9f8569becd5c52
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-05-23 02:27:49 -0500

    add eslint
    fixed linters

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+// eslint-disable-next-line camelcase
 const menu_btn = document.querySelector('.hamburger');
+// eslint-disable-next-line camelcase
 const mobile_menu = document.querySelector('.mobile-nav');
 
-menu_btn.addEventListener('click', function () {
-menu_btn.classList.toggle('is-active');
-mobile_menu.classList.toggle('is-active');
+menu_btn.addEventListener('click', () => {
+  menu_btn.classList.toggle('is-active');
+  mobile_menu.classList.toggle('is-active');
 });
